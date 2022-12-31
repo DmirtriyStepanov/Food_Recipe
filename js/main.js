2af654298c994e31092c754cf73da27eff48fbd6
GreyStone601 <dmitr97stepanov@gmail.com>
2022-12-31 17:53:10 +0300

    editing styles and creating a star rating of a dish

--- js/main.js
+++ js/main.js
@@ -31,14 +31,28 @@ function generateHTML(results) {
                 <div class="flex-container">
                     <h1 class="title">${result.recipe.label}</h1>
                     <div>
-                        <p class="item-data">${result.recipe.cuisineType}</p>
-                        <p class="item-data">${result.recipe.mealType}</p> 
-                        <p class="item-data">${result.recipe.dietLabels.length > 0
+                        <p class="item-data">cuisine: ${result.recipe.cuisineType}</p>
+                        <p class="item-data">meal: ${result.recipe.mealType}</p> 
+                        <p class="item-data">diet: ${result.recipe.dietLabels.length > 0
                             ? result.recipe.dietLabels : "unknown"}
                         </p>
-                        <p class="item-data">${result.recipe.calories.toFixed(2)} kcal</p>
+                        <p class="item-data">calories: ${result.recipe.calories.toFixed(2)} kcal</p>
                     </div>
-                    <a class="view-btn" target="_blank" href="${result.recipe.url}">View Recipe</a>
+                    <div>
+                        <div class="rating-area">
+                            <input type="radio" id="star-5" name="rating" value="5">
+                            <label for="star-5" title="Оценка «5»"></label>	
+                            <input type="radio" id="star-4" name="rating" value="4">
+                            <label for="star-4" title="Оценка «4»"></label>    
+                            <input type="radio" id="star-3" name="rating" value="3">
+                            <label for="star-3" title="Оценка «3»"></label>  
+                            <input type="radio" id="star-2" name="rating" value="2">
+                            <label for="star-2" title="Оценка «2»"></label>    
+                            <input type="radio" id="star-1" name="rating" value="1">
+                            <label for="star-1" title="Оценка «1»"></label>
+                        </div>
+                        <a class="view-btn" target="_blank" href="${result.recipe.url}">View Recipe</a>
+                    </div>
                 </div>
             </div>
         `;
